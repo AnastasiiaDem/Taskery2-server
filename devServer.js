736079df26867ae749f8e5105712c456a329228d
AnastasiiaDem <85587393+AnastasiiaDem@users.noreply.github.com>
2023-06-20 13:59:44 -0400

    simplify

--- devServer.js
+++ devServer.js
@@ -1,10 +1,8 @@
 const app = require("./app");
 const routes = require("./routes/router");
 
-app.use("/", routes);
-/* app.use("/api/", routes);  //for API backend*/
+app.use("/api/", routes);
 
-//start server locally
 app.listen(3000,function () {
     console.log("Server started. Go to http://localhost:3000/");
 });
